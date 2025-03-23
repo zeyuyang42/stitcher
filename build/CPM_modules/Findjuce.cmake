229a27dd37af7740df386126b6abd9f0fdb1b6ba
Zeyu Yang <213153676zsq@gmail.com>
2025-03-23 20:20:55 +0100

    init commit

--- build/CPM_modules/Findjuce.cmake
+++
@@ -1,3 +0,0 @@
-include("/Users/zeyuyang42/Documents/workspace/stitcher/build/cmake/CPM_0.34.0.cmake")
-CPMAddPackage(NAME;juce;GIT_REPOSITORY;https://github.com/juce-framework/JUCE.git;GIT_TAG;origin/master)
-set(juce_FOUND TRUE)
